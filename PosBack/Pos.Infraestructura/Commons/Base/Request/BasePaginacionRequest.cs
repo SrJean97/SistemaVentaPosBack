fc9d1ea827149bbc40c79904fa3ca5b2fa5d5c4c
SrJean97 <jheancarlos1997123@gmail.com>
2023-05-28 08:59:55 -0500

    Agregada clase base para la paginación

---
+++ PosBack/Pos.Infraestructura/Commons/Base/Request/BasePaginacionRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pos.Infraestructura.Commons.Base.Request
+{
+    public class BasePaginacionRequest
+    {
+        public int NumPagina { get; set; } = 1;
+        public int NumRegistrosPorPagina { get; set; } = 10;
+        public readonly int NumMaxRegistrosPorPagina = 50;
+        public string? Ordenar { get; set; } = "asc";
+        public string? AtributoPorCualOrdenar { get; set; } = null;
+
+        //Especiicar el registro inicial.
+        public int RegistroInicial
+        {
+            get => NumRegistrosPorPagina;
+            set
+            {
+                NumRegistrosPorPagina = value > NumMaxRegistrosPorPagina ? NumMaxRegistrosPorPagina : value;
+            }
+        }
+    }
+}
