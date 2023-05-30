1b54e7a679eaaefd33b0fc8400912960bd0fc845
SrJean97 <jheancarlos1997123@gmail.com>
2023-05-30 06:21:19 -0500

    Agregado primer controller para listar las categorias con filtro funcionando al 100%

---
+++ PosBack/Pos.Api/Controllers/Categoria/CategoriaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pos.Aplicacion.Interfaces;
+using Pos.Infraestructura.Commons.Base.Request;
+
+namespace Pos.Api.Controllers.Categoria
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        private readonly ICategoriaAplicacion _categoriaAplicacion;
+
+        public CategoriaController(ICategoriaAplicacion categoriaAplicacion)
+        {
+            _categoriaAplicacion = categoriaAplicacion;
+        }
+
+        //Primer método para listar las categorias con filtro
+        [HttpPost]
+        public async Task<IActionResult> ListaCategoriasConFiltro([FromBody] BaseFiltrosRequest filtros)
+        {
+            var response = await _categoriaAplicacion.ListarCategoriasConFiltro(filtros);
+            return Ok(response);
+        }
+    }
+}
