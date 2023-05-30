1ae1e354be9da9535d300790d28f0882b027b35d
SrJean97 <jheancarlos1997123@gmail.com>
2023-05-30 05:53:49 -0500

    Agregado servicio listado con filtro de categorias y creada clase para los mapeos

---
+++ PosBack/Pos.Aplicacion/Mappers/CategoriaMappingProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Pos.Aplicacion.Dtos.Request;
+using Pos.Aplicacion.Dtos.Response;
+using Pos.Dominio.Entidades;
+using Pos.Utilidades.Constantes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pos.Aplicacion.Mappers
+{
+    public class CategoriaMappingProfile : Profile
+    {
+        public CategoriaMappingProfile()
+        {
+            //Primer mapeo
+            CreateMap<Category, CategoriaResponseDto>()
+                .ForMember(x => x.EstadoDescriptivo, x => x.MapFrom(y => y.State.Equals((int)CategoriaEstados.Activo) ? "Activo" : "Inactivo"))
+                .ReverseMap();
+
+            //Segundo mapeo
+            
+        }
+    }
+}
---
+++ PosBack/Pos.Aplicacion/Servicios/CategoriaServicioAplicacion.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using Pos.Aplicacion.Commons.Base;
+using Pos.Aplicacion.Dtos.Request;
+using Pos.Aplicacion.Dtos.Response;
+using Pos.Aplicacion.Interfaces;
+using Pos.Aplicacion.Validaciones.Categoria;
+using Pos.Infraestructura.Commons.Base.Request;
+using Pos.Infraestructura.Commons.Base.Response;
+using Pos.Infraestructura.Persistencia.Interfaces;
+using Pos.Utilidades.Constantes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pos.Aplicacion.Servicios
+{
+    public class CategoriaServicioAplicacion : ICategoriaAplicacion
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly CategoriaValidaciones _validaciones;
+
+        public CategoriaServicioAplicacion(IUnitOfWork unitOfWork, IMapper mapper, CategoriaValidaciones validaciones)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _validaciones = validaciones;
+        }
+
+        public async Task<BaseResponse<BaseEntidadResponse<CategoriaResponseDto>>> ListarCategoriasConFiltro(BaseFiltrosRequest filtros)
+        {
+            var response = new BaseResponse<BaseEntidadResponse<CategoriaResponseDto>>();
+            var categoriasFiltradas = await _unitOfWork.CategoriaRepositorio.ListaCategoriasConFiltro(filtros);
+
+            if (categoriasFiltradas is not null)
+            {
+                response.IsSuccess= true;
+                response.Data = _mapper.Map<BaseEntidadResponse<CategoriaResponseDto>>(categoriasFiltradas);
+                response.Mensaje = MensajeRespuestas.MENSAJE_CONSULTA;
+            }
+            else
+            {
+                response.IsSuccess= false;
+                response.Mensaje = MensajeRespuestas.MENSAJE_QUERY_VACIO;
+            }
+            return response;
+        }
+
+        public Task<BaseResponse<IEnumerable<CategoriaSelectResponseDto>>> ListarCategoriasSinFiltro()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResponse<CategoriaResponseDto>> BuscarCategoriaxId(int categoriaId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResponse<bool>> RegistrarCategoria(CategoriaRequestDto requestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResponse<bool>> EditarCategoria(int categoriaId, CategoriaRequestDto requestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResponse<bool>> EliminarCategoria(int CategoriaId)
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
