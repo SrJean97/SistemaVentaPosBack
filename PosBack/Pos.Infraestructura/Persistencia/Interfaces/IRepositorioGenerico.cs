f93dc92c772fdaa15a077833081a27ddd8c97506
SrJean97 <jheancarlos1997123@gmail.com>
2023-06-01 07:54:20 -0500

    modificada interfaz de repositorio generico

--- PosBack/Pos.Infraestructura/Persistencia/Interfaces/IRepositorioGenerico.cs
+++ PosBack/Pos.Infraestructura/Persistencia/Interfaces/IRepositorioGenerico.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pos.Dominio.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,13 @@ using System.Threading.Tasks;
 
 namespace Pos.Infraestructura.Persistencia.Interfaces
 {
-    public interface IRepositorioGenerico<T> where T : class
+    public interface IRepositorioGenerico<T> where T : EntidadBase
     {
         //Este repositorio generico va a contener el crud común entre todas las clases conocidas.
+        //Acá el único repositorio que no se va a encontrar es el de buscado por iltros de categoría, ese le pertenece a la categoría y solo a la categoría como tal.
+
+        Task<IEnumerable<T>> ObtenerTodoAsync();
+
+        Task<T> ConsultarAsync(int id);
     }
 }
