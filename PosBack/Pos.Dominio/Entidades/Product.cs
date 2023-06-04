da1e9e4949f57c7a919470d101429105de707190
SrJean97 <jheancarlos1997123@gmail.com>
2023-06-04 13:32:10 -0500

    Implementado repositorio generico en el proyecto

--- PosBack/Pos.Dominio/Entidades/Product.cs
+++ PosBack/Pos.Dominio/Entidades/Product.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Pos.Dominio.Entidades
 {
-    public partial class Product
+    public partial class Product : EntidadBase
     {
         public Product()
         {
@@ -11,7 +11,6 @@ namespace Pos.Dominio.Entidades
             SaleDetails = new HashSet<SaleDetail>();
         }
 
-        public int ProductId { get; set; }
         public string? Code { get; set; }
         public string? Name { get; set; }
         public int Stock { get; set; }
@@ -19,13 +18,6 @@ namespace Pos.Dominio.Entidades
         public decimal SellPrice { get; set; }
         public int CategoryId { get; set; }
         public int ProviderId { get; set; }
-        public int State { get; set; }
-        public int AuditCreateUser { get; set; }
-        public DateTime AuditCreateDate { get; set; }
-        public int? AuditUpdateUser { get; set; }
-        public DateTime? AuditUpdateDate { get; set; }
-        public int? AuditDeleteUser { get; set; }
-        public DateTime? AuditDeleteDate { get; set; }
 
         public virtual Category Category { get; set; } = null!;
         public virtual Provider Provider { get; set; } = null!;
